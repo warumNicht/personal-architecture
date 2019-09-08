0d37c827be263c6f348b1c6cd93a2c1c159f66b5
warumNicht <andrey_st_minev@yahoo.com>
2019-09-08 21:33:15 +0300

    fist commit

---
+++ src/main/java/architecture/PersonalSiteApplication.java
@@ -0,0 +1,13 @@
+package architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalSiteApplication.class, args);
+    }
+
+}
