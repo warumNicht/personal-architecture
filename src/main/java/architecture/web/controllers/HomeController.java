3094736f909c8a945e33970701de2d9fec6e94ef
warumNicht <andrey_st_minev@yahoo.com>
2019-09-08 21:40:11 +0300

    added home controller

---
+++ src/main/java/architecture/web/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package architecture.web.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping("")
+    public String index(){
+        return "Under construction";
+    }
+}
