eeecfff274d4ef47cece45411247a2be325e3884
warumNicht <andrey_st_minev@yahoo.com>
2019-09-08 21:51:47 +0300

    some words

--- src/main/java/architecture/web/controllers/HomeController.java
+++ src/main/java/architecture/web/controllers/HomeController.java
@@ -3,6 +3,7 @@ package architecture.web.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/")
@@ -10,6 +11,12 @@ public class HomeController {
 
     @GetMapping("")
     public String index(){
-        return "Under construction";
+        return "<h1 style=\"color: red;\">Under construction</h1>";
+    }
+
+    @GetMapping("/home")
+    public ModelAndView getIndex(ModelAndView modelAndView){
+        modelAndView.setViewName("index");
+        return modelAndView;
     }
 }
