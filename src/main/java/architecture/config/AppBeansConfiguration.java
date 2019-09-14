18e3788c6ea1241d3d7bf7a1e212ab409c12eabb
warumNicht <andrey_st_minev@yahoo.com>
2019-09-14 15:40:39 +0300

    cookie resolver

--- src/main/java/architecture/config/AppBeansConfiguration.java
+++ src/main/java/architecture/config/AppBeansConfiguration.java
@@ -3,6 +3,7 @@ package architecture.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -13,9 +14,11 @@ public class AppBeansConfiguration {
 
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.US);
-        return slr;
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setDefaultLocale(Locale.US);
+        cookieLocaleResolver.setCookieName("lang");
+        cookieLocaleResolver.setCookieMaxAge(500);
+        return cookieLocaleResolver;
     }
 
     @Bean
