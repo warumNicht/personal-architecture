5a5c60509b42d0b2144b0c0be8825c8146d666db
warumNicht <andrey_st_minev@yahoo.com>
2019-09-17 20:58:00 +0300

    create article page

---
+++ src/main/java/architecture/domain/ArticleBindingModel.java
@@ -0,0 +1,31 @@
+package architecture.domain;
+
+public class ArticleBindingModel {
+    private String country;
+    private String title;
+    private String content;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
--- src/main/java/architecture/web/controllers/HomeController.java
+++ src/main/java/architecture/web/controllers/HomeController.java
@@ -1,13 +1,14 @@
 package architecture.web.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import architecture.domain.ArticleBindingModel;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/")
@@ -23,5 +24,24 @@ public class HomeController {
     public String index(HttpServletRequest request) {
         LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
         return "<h1 style=\"color: red;\">Under construction</h1>";
+    }
+
+    @GetMapping("/create")
+    public ModelAndView createArticle(@ModelAttribute(name = "articleBinding") ArticleBindingModel model, ModelAndView modelAndView) {
+        modelAndView.addObject("articleBinding",model);
+        modelAndView.setViewName("create");
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    private ModelAndView createArticlePost(@Valid @ModelAttribute(name = "articleBinding") ArticleBindingModel model,
+                                           BindingResult bindingResult, ModelAndView modelAndView){
+        if(bindingResult.hasErrors()){
+            modelAndView.addObject("articleBinding",model);
+            modelAndView.setViewName("create");
+            return modelAndView;
+        }
+        modelAndView.setViewName("redirect:/");
+        return modelAndView;
     }
 }
