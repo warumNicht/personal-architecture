49839e6a100ec4df19864d24a1845dddb3093283
warumNicht <andrey_st_minev@yahoo.com>
2019-09-19 22:04:50 +0300

    persisted

--- src/main/java/architecture/web/controllers/HomeController.java
+++ src/main/java/architecture/web/controllers/HomeController.java
@@ -1,6 +1,8 @@
 package architecture.web.controllers;
 
 import architecture.domain.ArticleBindingModel;
+import architecture.domain.CountryCodes;
+import architecture.domain.entities.LocalisedArticleContent;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import architecture.domain.entities.Article;
@@ -56,15 +58,27 @@ public class HomeController {
             modelAndView.setViewName("create");
             return modelAndView;
         }
-//        Article article=new Article();
-//        article.setDate(new Date());
-//        article.getText().put("BG", model.getTitle());
-//        article.getText().put("FR", "francais");
-//
-//        article.getContent().put("EN", model.getContent());
-//        article.getContent().put("DE", "deutsch");
-//        article.getContent().put("ES", "espanol");
-//        this.articleRepo.saveAndFlush(article);
+        Article article=new Article();
+        article.setDate(new Date());
+        LocalisedArticleContent contentFr = new LocalisedArticleContent("Entreprise", "Societe Nationale des Chemins de fer");
+        LocalisedArticleContent contentDe = new LocalisedArticleContent("Unternehmen", "Deutsche Bahn");
+        LocalisedArticleContent contentEs = new LocalisedArticleContent("Empresa", "Ciudad de las artes y las ciencias");
+        LocalisedArticleContent contentEn = new LocalisedArticleContent("Ltd", "Sofilco");
+        LocalisedArticleContent contentBg = new LocalisedArticleContent("Предприятие", "Български Държавни Железници");
+
+        article.getLocalContent().put(CountryCodes.DE, contentDe);
+        article.getLocalContent().put(CountryCodes.ES, contentEs);
+        article.getLocalContent().put(CountryCodes.FR, contentFr);
+        article=this.articleRepo.saveAndFlush(article);
+
+        Article article2=new Article();
+        article2.setDate(new Date());
+
+        article2.getLocalContent().put(CountryCodes.BG, contentBg);
+        article2.getLocalContent().put(CountryCodes.EN, contentEn);
+        article2.getLocalContent().put(CountryCodes.FR, contentFr);
+        article2=this.articleRepo.saveAndFlush(article2);
+
         modelAndView.setViewName("redirect:/");
         return modelAndView;
     }
