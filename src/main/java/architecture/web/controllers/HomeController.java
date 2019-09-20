351d8d1ad9887937df0694c5f8ae18a71963526f
warumNicht <andrey_st_minev@yahoo.com>
2019-09-20 22:27:26 +0300

    rem imports

--- src/main/java/architecture/web/controllers/HomeController.java
+++ src/main/java/architecture/web/controllers/HomeController.java
@@ -11,10 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.support.RequestContextUtils;
-
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
@@ -63,23 +60,28 @@ public class HomeController {
         }
         Article article=new Article();
         article.setDate(new Date());
-        LocalisedArticleContent contentFr = new LocalisedArticleContent("Entreprise", "Societe Nationale des Chemins de fer");
-        LocalisedArticleContent contentDe = new LocalisedArticleContent("Unternehmen", "Deutsche Bahn");
-        LocalisedArticleContent contentEs = new LocalisedArticleContent("Empresa", "Ciudad de las artes y las ciencias");
-        LocalisedArticleContent contentEn = new LocalisedArticleContent("Ltd", "Sofilco");
-        LocalisedArticleContent contentBg = new LocalisedArticleContent("Предприятие", "Български Държавни Железници");
+        LocalisedArticleContent contentFr = new LocalisedArticleContent("Louvre", "Musee du Louvre");
+        LocalisedArticleContent contentFr2 = new LocalisedArticleContent("Palais royal", "Champs Elysees");
+        LocalisedArticleContent contentDe = new LocalisedArticleContent("Brandenburg", "Potsdamer Platz");
+        LocalisedArticleContent contentEs = new LocalisedArticleContent("Calle Vicente", "plaza Mayor");
+        LocalisedArticleContent contentEs2 = new LocalisedArticleContent("San Isidre", "Fiesta del verano");
+        LocalisedArticleContent contentEn = new LocalisedArticleContent("The king", "Henri VIII");
+        LocalisedArticleContent contentBg = new LocalisedArticleContent("УАСГ", "Университет по архитектура строителство и геодезия");
+        LocalisedArticleContent contentBg2 = new LocalisedArticleContent("Проекти", "Фирма рога и копита");
 
         article.getLocalContent().put(CountryCodes.DE, contentDe);
         article.getLocalContent().put(CountryCodes.ES, contentEs);
         article.getLocalContent().put(CountryCodes.FR, contentFr);
+        article.getLocalContent().put(CountryCodes.BG, contentBg);
         article=this.articleRepo.saveAndFlush(article);
 
         Article article2=new Article();
         article2.setDate(new Date());
 
-        article2.getLocalContent().put(CountryCodes.BG, contentBg);
+        article2.getLocalContent().put(CountryCodes.BG, contentBg2);
         article2.getLocalContent().put(CountryCodes.EN, contentEn);
-        article2.getLocalContent().put(CountryCodes.FR, contentFr);
+        article2.getLocalContent().put(CountryCodes.FR, contentFr2);
+        article2.getLocalContent().put(CountryCodes.ES, contentEs2);
         article2=this.articleRepo.saveAndFlush(article2);
 
         modelAndView.setViewName("redirect:/");
