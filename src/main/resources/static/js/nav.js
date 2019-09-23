7007d1f38853c1dffc29667c078b540de7c20cf6
warumNicht <andrey_st_minev@yahoo.com>
2019-09-23 13:06:54 +0300

    nav functions  -> nav.js

---
+++ src/main/resources/static/js/nav.js
@@ -0,0 +1,22 @@
+$('ul.navbar-nav li.dropdown').hover(function () {
+    $(this).find(".dropdown-menu").stop(true, true).delay(200).fadeIn(500);
+}, function () {
+    $(this).find(".dropdown-menu").stop(true, true).delay(200).fadeOut(500);
+});
+
+$(document).ready(function () {
+    $("#locales").change(function () {
+        let selectedOption = $('#locales').val();
+        if (selectedOption != '') {
+            location.replace('?lang=' + selectedOption);
+        }
+    });
+});
+
+$(document).ready(function () {
+    let url = location.href;
+    let parts = url.split('?lang=');
+    if (parts.length > 1) {
+        location.assign(parts[0]);
+    }
+});
