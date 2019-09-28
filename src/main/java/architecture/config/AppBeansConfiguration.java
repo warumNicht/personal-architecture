3d361f91470950e01286487c37e52be3526fc690
warumNicht <andrey_st_minev@yahoo.com>
2019-09-28 15:12:41 +0300

    rem unused

--- src/main/java/architecture/config/AppBeansConfiguration.java
+++ src/main/java/architecture/config/AppBeansConfiguration.java
@@ -11,19 +11,6 @@ import java.util.Locale;
 @Configuration
 public class AppBeansConfiguration {
 
-
-//    @Bean(name = "messageSource")
-//    public MessageSource getMessageResource()  {
-//        ReloadableResourceBundleMessageSource messageResource= new ReloadableResourceBundleMessageSource();
-//
-//        // Read i18n/messages_xxx.properties file.
-//        // For example: i18n/messages_en.properties
-//        messageResource.setBasename("classpath:i18n/messages");
-////        messageResource.setDefaultEncoding("UTF-8");
-//        return messageResource;
-//    }
-
-
     @Bean
     public  LocaleResolver localeResolver(){
         CustomLocalResolver customLocalResolver = new CustomLocalResolver();
@@ -33,11 +20,6 @@ public class AppBeansConfiguration {
         customLocalResolver.setCookieMaxAge(60*60);
         return customLocalResolver;
     }
-//
-//    @Bean
-//    public DispatcherServlet dispatcherServlet() {
-//        return new DispatcherServlet();
-//    }
 
     @Bean
     public ServletRegistrationBean dispatcherRegistration(DispatcherServlet dispatcherServlet) {
@@ -48,14 +30,5 @@ public class AppBeansConfiguration {
         registration.setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_REGISTRATION_BEAN_NAME);
         return registration;
     }
-
-//    @Bean
-//    public ServletRegistrationBean dispatcherServletRegistration(DispatcherServlet dispatcherServlet) {
-//        ServletRegistrationBean registration = new ServletRegistrationBean(
-//                dispatcherServlet, "/", "/bg/", "/de/", "/en/", "/fr/", "/es/");
-//        registration.setName(DispatcherServletAutoConfiguration.DEFAULT_DISPATCHER_SERVLET_REGISTRATION_BEAN_NAME);
-//        return registration;
-//    }
-
 
 }
