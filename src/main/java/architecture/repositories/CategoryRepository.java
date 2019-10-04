53b2b54461b47c82b7e5160f708096468e4f54ed
warumNicht <andrey_st_minev@yahoo.com>
2019-10-04 22:06:15 +0300

    removed

--- src/main/java/architecture/repositories/CategoryRepository.java
+++ src/main/java/architecture/repositories/CategoryRepository.java
@@ -10,14 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    @Query(value = "SELECT a.id, a.date, value(m)" +
-            "FROM Article a " +
-            "JOIN a.localContent m " +
-            "ON key(m) = ( SELECT max(key(n)) FROM Article  b " +
-            "JOIN b.localContent n " +
-            "WHERE a.id=b.id AND (KEY(n) = :countryCode OR KEY(n) = :defaultCode )) " )
-    Object[] getAllNestedSelect(@Param(value = "countryCode") CountryCodes countryCode, @Param(value = "defaultCode") CountryCodes defaultCode);
-
     @Query(value = "SELECT c.id, value(m) FROM Category c " +
             "JOIN c.localCategoryNames m " +
             "ON key(m) = ( SELECT max(key(n)) FROM Category b " +
