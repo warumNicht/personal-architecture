f649c36f3a44b8c5282a49b335d3a7de954b1ee5
warumNicht <andrey_st_minev@yahoo.com>
2019-10-22 21:01:29 +0300

    rem

--- src/main/java/architecture/services/ArticleServiceImpl.java
+++ src/main/java/architecture/services/ArticleServiceImpl.java
@@ -11,7 +11,6 @@ import architecture.services.interfaces.ArticleService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
--- src/main/java/architecture/web/controllers/ProjectController.java
+++ src/main/java/architecture/web/controllers/ProjectController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
