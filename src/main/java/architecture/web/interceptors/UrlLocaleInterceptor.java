18edaff2f0958fadf683c37b21c91a2b4536651d
warumNicht <andrey_st_minev@yahoo.com>
2019-10-31 22:48:56 +0200

    rem line

--- src/main/java/architecture/web/interceptors/UrlLocaleInterceptor.java
+++ src/main/java/architecture/web/interceptors/UrlLocaleInterceptor.java
@@ -12,7 +12,6 @@ public class UrlLocaleInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-
         System.out.print("Url interceptor ");
         System.out.println(request.getRequestURI());
         String requestMethod = request.getMethod();
