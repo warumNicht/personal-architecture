0c1b96915d0a35aa8ded6514480cc3d9677b7794
warumNicht <andrey_st_minev@yahoo.com>
2019-11-11 22:13:58 +0200

    fetch with xmlHttpRequest

--- src/main/resources/static/js/fetch-functions.js
+++ src/main/resources/static/js/fetch-functions.js
@@ -1,12 +1,25 @@
-function fetchCategories( selectElement) {
-    fetch('/fetch/categories/all')
-        .then((response) => response.json())
-        .then((json) => {
+function fetchCategories(selectElement) {
+    // fetch('/fetch/categories/all')
+    //     .then((response) => response.json())
+    //     .then((json) => {
+    //         json.forEach((category) => {
+    //             selectElement.append('<option value="'+ category.id +'">'+ category.name +'</option>');
+    //         });
+    //     })
+    //     .catch((err) => console.log(err));
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', '/fetch/categories/all', true);
+    xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            const json = JSON.parse(this.response);
             json.forEach((category) => {
-                selectElement.append('<option value="'+ category.id +'">'+ category.name +'</option>');
+                selectElement.append('<option value="' + category.id + '">' + category.name + '</option>');
             });
-        })
-        .catch((err) => console.log(err));
+
+        }
+    };
+    xhttp.send(null);
 }
 
 export {fetchCategories}
