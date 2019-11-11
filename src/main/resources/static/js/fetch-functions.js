1e7e3e7cfd4819ba7421c2b2f0224289429666a0
warumNicht <andrey_st_minev@yahoo.com>
2019-11-11 22:28:04 +0200

    fetch IE

--- src/main/resources/static/js/fetch-functions.js
+++ src/main/resources/static/js/fetch-functions.js
@@ -1,33 +1,13 @@
-import {sendXmlHttpRequest} from "./http-requests.js";
-
 function fetchCategories(selectElement) {
-    // function callback(request, selectElement) {
-    //
-    //     console.log(request.response);
-    //     const data =request.response;
-    //     // data.forEach((category) => {
-    //     //     console.log(category);
-    //         selectElement.append('<option value="' + '">' + 'darhbytq354' + '</option>');
-    //     // });
-    // }
-    const xhttp = new XMLHttpRequest();
-    xhttp.open('GET', '/fetch/categories/all', true);
-    xhttp.setRequestHeader('Content-type','application/json; charset=utf-8');
-    xhttp.onreadystatechange= function(){
-        if(this.readyState==4){
-            selectElement.append('<option value="' + '">' + 'darhbytq354' + '</option>');
-        }
-    };
-    xhttp.send(null);
-    // sendXmlHttpRequest('GET', '/fetch/categories/all', selectElement ,callback)
-    // fetch('/fetch/categories/all')
-    //     .then((response) => response.json())
-    //     .then((json) => {
-    //         json.forEach((category) => {
-    //             selectElement.append('<option value="'+ category.id +'">'+ category.name +'</option>');
-    //         });
-    //     })
-    //     .catch((err) => console.log(err));
+
+    fetch('/fetch/categories/all')
+        .then((response) => response.json())
+        .then((json) => {
+            json.forEach((category) => {
+                selectElement.append('<option value="'+ category.id +'">'+ category.name +'</option>');
+            });
+        })
+        .catch((err) => console.log(err));
 }
 
 export {fetchCategories}
