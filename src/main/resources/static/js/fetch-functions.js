b4eb94297e9a6ea9c763ddea70f7038dee338baa
warumNicht <andrey_st_minev@yahoo.com>
2019-11-12 22:45:44 +0200

    restored to fetch

--- src/main/resources/static/js/fetch-functions.js
+++ src/main/resources/static/js/fetch-functions.js
@@ -1,13 +1,12 @@
 function fetchCategories(selectElement) {
-    const xhttp = new XMLHttpRequest();
-    xhttp.open('GET', '/fetch/categories/all', true);
-    xhttp.setRequestHeader('Content-type','application/json; charset=utf-8');
-    xhttp.onreadystatechange= function(){
-        if(this.readyState==4){
-            selectElement.append('<option value="' + '">' + 'darhbytq354' + '</option>');
-        }
-    };
-    xhttp.send(null);
+    fetch('/fetch/categories/all')
+        .then((response) => response.json())
+        .then((json) => {
+            json.forEach((category) => {
+                selectElement.append('<option value="'+ category.id +'">'+ category.name +'</option>');
+            });
+        })
+        .catch((err) => console.log(err));
 }
 
 export {fetchCategories}
