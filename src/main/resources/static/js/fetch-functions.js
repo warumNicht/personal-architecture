7bc130f3414be5a259039fb999312448fc1ba232
warumNicht <andrey_st_minev@yahoo.com>
2019-11-12 22:31:04 +0200

    working xmlHttpRequest

--- src/main/resources/static/js/fetch-functions.js
+++ src/main/resources/static/js/fetch-functions.js
@@ -1,6 +1,17 @@
 function fetchCategories(selectElement) {
 
-console.log('dddd');
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET', '/fetch/categories/all', true);
+    xhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            const json = JSON.parse(this.response);
+            json.forEach(function(category){
+                selectElement.append('<option value="' + category.id + '">' + category.name + '</option>');
+            });
+        }
+    };
+    xhttp.send(null);
 }
 
 export {fetchCategories}
