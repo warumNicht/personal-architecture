da7b5eaf01d427b4e0a1df53faf9a4c1f7b6666c
warumNicht <andrey_st_minev@yahoo.com>
2019-11-13 20:05:46 +0200

    changes

--- src/main/resources/static/js/nav.js
+++ src/main/resources/static/js/nav.js
@@ -14,7 +14,14 @@ $(document).ready(function () {
         }
     });
     const select = $('#select-categories');
-    fetchCategories(select);
+    fetch('/fetch/categories/all')
+        .then((response) => response.json())
+        .then(function(json){
+            json.forEach(function(category){
+                select.append('<option value="'+ category.id +'">'+ category.name +'</option>');
+            });
+        })
+        .catch((err) => console.log(err));
     select.change(function () {
         let selectedOption = select.val();
         if (selectedOption != '') {
