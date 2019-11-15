23f144970efcce9afa6ba8ee173c8c2f9548c68f
warumnicht <andrey_st_minev@yahoo.com>
2019-11-15 13:30:07 +0200

    Added polyfill IE nodelist.foreach

--- src/main/resources/static/js/article-edit-lang.js
+++ src/main/resources/static/js/article-edit-lang.js
@@ -1,9 +1,10 @@
 import {sendXmlHttpRequest, createJsonFromInputs} from "./http-requests.js";
 
-$(document).ready(()=>{
+$(document).ready(function(){
     const button=document.getElementById("submit-button");
     button.onclick=function () {
         const inputs=document.querySelectorAll('main input');
+        console.log(inputs);
         const data = createJsonFromInputs(inputs);
         const json = JSON.stringify(data);
 
