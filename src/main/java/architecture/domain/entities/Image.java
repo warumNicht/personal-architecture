e03527bd3f3fce4bf96e58fc7c30480f70c0dc8a
warumnicht <andrey_st_minev@yahoo.com>
2019-11-18 12:40:42 +0200

    changes

--- src/main/java/architecture/domain/entities/Image.java
+++ src/main/java/architecture/domain/entities/Image.java
@@ -1,27 +1,27 @@
 package architecture.domain.entities;
 
+import architecture.domain.CountryCodes;
+
 import javax.persistence.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @Entity
 @Table(name = "images")
 public class Image extends BaseEntity {
-    @Column(name = "image_name")
-    private String name;
-
     @Column(name = "image_url")
     private String url;
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "image_names", joinColumns = @JoinColumn(name = "image_id"))
+    @MapKeyColumn(name = "country_code")
+    @MapKeyEnumerated(EnumType.STRING)
+    private Map<CountryCodes, String> localImageNames = new HashMap<>();
 
     @OneToOne
     @JoinColumn(name = "article_id", referencedColumnName = "id")
     private Article article;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String getUrl() {
         return url;
@@ -29,6 +29,14 @@ public class Image extends BaseEntity {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public Map<CountryCodes, String> getLocalImageNames() {
+        return localImageNames;
+    }
+
+    public void setLocalImageNames(Map<CountryCodes, String> localImageNames) {
+        this.localImageNames = localImageNames;
     }
 
     public Article getArticle() {
--- src/main/java/architecture/domain/models/serviceModels/ImageServiceModel.java
+++ src/main/java/architecture/domain/models/serviceModels/ImageServiceModel.java
@@ -1,17 +1,13 @@
 package architecture.domain.models.serviceModels;
 
+import architecture.domain.CountryCodes;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ImageServiceModel extends BaseServiceModel{
-    private String name;
     private String url;
+    private Map<CountryCodes, String> localImageNames = new HashMap<>();
     private ArticleServiceModel article;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String getUrl() {
         return url;
@@ -19,6 +15,14 @@ public class ImageServiceModel extends BaseServiceModel{
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public Map<CountryCodes, String> getLocalImageNames() {
+        return localImageNames;
+    }
+
+    public void setLocalImageNames(Map<CountryCodes, String> localImageNames) {
+        this.localImageNames = localImageNames;
     }
 
     public ArticleServiceModel getArticle() {
