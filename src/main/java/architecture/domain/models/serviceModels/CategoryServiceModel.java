6be855b546e8b37591ffeb485723254c10effaf0
warumNicht <andrey_st_minev@yahoo.com>
2019-11-23 20:55:24 +0200

    baseModel

--- src/main/java/architecture/domain/models/serviceModels/CategoryServiceModel.java
+++ src/main/java/architecture/domain/models/serviceModels/CategoryServiceModel.java
@@ -1,11 +1,11 @@
 package architecture.domain.models.serviceModels;
 
 import architecture.domain.CountryCodes;
-
+import architecture.domain.models.BaseModel;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CategoryServiceModel extends BaseServiceModel {
+public class CategoryServiceModel extends BaseModel {
     private Map<CountryCodes, String> localCategoryNames = new HashMap<>();
 
     public Map<CountryCodes, String> getLocalCategoryNames() {
