a8dc3bb88784a6d65130167c592917ba6c2b6cc4
warumNicht <andrey_st_minev@yahoo.com>
2019-11-24 16:06:54 +0200

    inline javascript

--- src/main/resources/static/js/article-add-image.js
+++ src/main/resources/static/js/article-add-image.js
@@ -1,4 +1,8 @@
 import {sendXmlHttpRequest, createJsonFromInputs} from "./http-requests.js";
+/*<![CDATA[*/
+const art = [[${article}]];  //Make sure this is bills and not bill as you have done it.
+console.log(art);
+/*]]>*/
 
 $(document).ready(function () {
     const button = document.getElementById("submit-button");
@@ -7,7 +11,7 @@ $(document).ready(function () {
         console.log(inputs);
         const data = createJsonFromInputs(inputs);
         const json = JSON.stringify(data);
-        
+
         sendXmlHttpRequest('PATCH', '/admin/articles/edit', json).then(function (res) {
                 console.log(res);
                 window.location = res;
