8b65133d0418c3ac8c6255bb8f07a9399c413fa4
warumnicht <andrey_st_minev@yahoo.com>
2019-12-05 09:53:53 +0100

    added js

---
+++ src/main/resources/static/js/article-edit.js
@@ -0,0 +1,24 @@
+import {sendXmlHttpRequest} from "./http-requests.js";
+
+$(document).ready(function () {
+//    const button = document.getElementById("submit-button");
+//    button.onclick = function () {
+//        const selectLang = document.getElementById('lang');
+//        const lang = selectLang.options[selectLang.selectedIndex].value;
+//        const data = {
+//            id: article.id,
+//            lang: lang,
+//            image: {
+//                url: document.getElementById('url').value,
+//                name: document.getElementById('name').value
+//            }
+//        };
+//        const json = JSON.stringify(data);
+//
+//        sendXmlHttpRequest('PUT', '/admin/articles/add-image', json).then(function (res) {
+//                console.log(res);
+//                window.location = res;
+//            }
+//        );
+//    };
+});
