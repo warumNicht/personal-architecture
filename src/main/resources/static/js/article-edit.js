8b5488b63ba51177aa59124380f666d49c67b93b
warumnicht <andrey_st_minev@yahoo.com>
2019-12-27 13:15:59 +0200

    apend images

--- src/main/resources/static/js/article-edit.js
+++ src/main/resources/static/js/article-edit.js
@@ -2,12 +2,25 @@ import {sendXmlHttpRequest} from "./http-requests.js";
 
 window.showImages = function showImages() {
     console.log('AAAAAAA');
+    const imageDiv = $("#image-container");
+    if(imageDiv.html()){
+    return;
+    }
     const urlParts=window.location.pathname.split('/');
     const articleId=urlParts[urlParts.length-1];
             sendXmlHttpRequest('GET', '/fetch/images/'+ articleId).then(function (res) {
-            const imageDiv = document.getElementById("image-container");
+
                     console.log(imageDiv);
-          
+                    res.forEach(function (image) {
+                           const currentImageDiv=$('<div></div>');
+                           currentImageDiv.append(`<img src="${image.url}">`);
+                           const names=image.localImageNames;
+                          for (var key in names) {
+                            console.log( key + " : " + names[key] );
+                            currentImageDiv.append(`<div><span>${key}</span> ${names[key]}</div>`);
+                          }
+                           imageDiv.append(currentImageDiv);
+                        });
                     console.log(res);
                 }
             );
