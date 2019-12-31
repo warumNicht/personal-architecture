6a4de369fb5eb55f1a89f22ef98d2b485dfdef95
warumnicht <andrey_st_minev@yahoo.com>
2019-12-31 10:32:39 +0200

    showed categories

--- src/main/resources/static/js/fetch-functions.js
+++ src/main/resources/static/js/fetch-functions.js
@@ -3,12 +3,11 @@ import {sendXmlHttpRequest} from "./http-requests.js";
 function fetchCategories(selectElement) {
     let urlParts = window.location.toString().split('/');
     let categoryId = urlParts[urlParts.length - 1];
-    console.log(categoryId)
 
     sendXmlHttpRequest('GET', '/fetch/categories/all').then(function (res) {
         res.forEach(function (category) {
-            const selected = categoryId == category.id ? 'selected ' : '';
-            selectElement.append('<option ' + selected + 'value="' + category.id + '">' + category.name + '</option>');
+            const isSelected = categoryId == category.id ? 'selected ' : '';
+            selectElement.append('<option ' + isSelected + 'value="' + category.id + '">' + category.name + '</option>');
         })
     }).catch(function (error) {
         console.log(error);
