4de64079f5b22666d1d27885041e26edfcc57b03
warumNicht <andrey_st_minev@yahoo.com>
2020-01-05 16:23:55 +0200

    uppercase validation

--- src/main/java/architecture/domain/models/bindingModels/CategoryCreateBindingModel.java
+++ src/main/java/architecture/domain/models/bindingModels/CategoryCreateBindingModel.java
@@ -3,12 +3,14 @@ package architecture.domain.models.bindingModels;
 import architecture.domain.CountryCodes;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class CategoryCreateBindingModel {
     @NotNull
     private CountryCodes country;
     @Size(min=8, max=30, message = "category.name.length")
+    @Pattern(regexp = "^[A-ZА-Я].*$",message = "begin-uppercase")
     private String name;
 
     public CountryCodes getCountry() {
