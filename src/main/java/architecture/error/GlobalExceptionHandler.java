b3ea747c08d697907381f2f31ad6caa4c8b0f0d9
warumnicht <andrey_st_minev@yahoo.com>
2020-01-06 13:19:17 +0200

    global exception handler

---
+++ src/main/java/architecture/error/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package architecture.error;
+
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    public static final String DEFAULT_ERROR_VIEW = "error/error";
+    public static final String CONTROLLER_ERROR_VIEW = "error/custom-error";
+
+//    @Order(value = 1)
+    @ExceptionHandler(value = ControllerError.class)
+    public ModelAndView
+    controllerErrorHandler(HttpServletRequest req, ControllerError e) throws Exception {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName(CONTROLLER_ERROR_VIEW);
+        return mav;
+    }
+
+//    @Order(value = 2)
+    @ExceptionHandler(value = Exception.class)
+    public ModelAndView
+    defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        // If the exception is annotated with @ResponseStatus rethrow it and let
+        // the framework handle it - like the OrderNotFoundException example
+        // at the start of this post.
+        // AnnotationUtils is a Spring Framework utility class.
+        if (AnnotationUtils.findAnnotation
+                (e.getClass(), ResponseStatus.class) != null)
+            throw e;
+
+        // Otherwise setup and send the user to a default error-view.
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName(DEFAULT_ERROR_VIEW);
+        return mav;
+    }
+
+
+}
