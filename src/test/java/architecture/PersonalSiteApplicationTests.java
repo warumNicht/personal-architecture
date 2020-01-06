d5a75426e63cb093c40b36dc14fa4634452014b3
warumNicht <andrey_st_minev@yahoo.com>
2020-01-06 22:06:28 +0200

    app class test

--- src/test/java/architecture/PersonalSiteApplicationTests.java
+++ src/test/java/architecture/PersonalSiteApplicationTests.java
@@ -1,16 +1,22 @@
 package architecture;
 
+import architecture.web.controllers.HomeController;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PersonalSiteApplicationTests {
+    @Autowired
+    private HomeController controller;
 
     @Test
     public void contextLoads() {
+        Assert.assertNotNull(this.controller);
     }
 
 }
