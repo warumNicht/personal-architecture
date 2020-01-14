d0f3871dd4bc60eb04fd074488813514452b45f0
warumnicht <andrey_st_minev@yahoo.com>
2020-01-14 10:17:21 +0200

    abstract article binding

--- src/main/java/architecture/domain/models/bindingModels/ArticleCreateBindingModel.java
+++ src/main/java/architecture/domain/models/bindingModels/ArticleCreateBindingModel.java
@@ -11,26 +11,10 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-public class ArticleCreateBindingModel {
+public class ArticleCreateBindingModel extends ArticleBindingModel{
     @NotNull
     @Min(value = 1)
     private Long categoryId;
-
-    @NotNull
-    @EnumValidator(enumClass = CountryCodes.class, message = "{country.nonexistent}")
-    private CountryCodes country;
-
-    @NotNull
-    @NotEmpty(message = "{value.empty}")
-    @Size(min = AppConstants.NAME_MIN_LENGTH, max = AppConstants.NAME_MAX_LENGTH, message = "{text.length.between}")
-    @BeginUppercase(allowEmpty = true)
-    private String title;
-
-    @NotNull
-    @NotEmpty(message = "{value.empty}")
-    @Size(min = AppConstants.DESCRIPTION_MIN_LENGTH, message = "{text.length.min}")
-    @BeginUppercase(allowEmpty = true)
-    private String content;
 
     @NotNull
     @Valid
@@ -46,30 +30,6 @@ public class ArticleCreateBindingModel {
 
     public void setCategoryId(Long categoryId) {
         this.categoryId = categoryId;
-    }
-
-    public CountryCodes getCountry() {
-        return country;
-    }
-
-    public void setCountry(CountryCodes country) {
-        this.country = country;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
     }
 
     public ImageBindingModel getMainImage() {
