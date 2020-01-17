674b7d01b93be6baa1b0f97fdbeda3e99c915fbe
warumnicht <andrey_st_minev@yahoo.com>
2020-01-17 15:10:28 +0200

    field errors url tests

--- src/test/java/architecture/integration/web/ArticleControllerIntegrationTests.java
+++ src/test/java/architecture/integration/web/ArticleControllerIntegrationTests.java
@@ -1,17 +1,16 @@
 package architecture.integration.web;
 
+import architecture.annotations.ImageBindingValidationEmpty;
 import architecture.constants.AppConstants;
 import architecture.constants.ViewNames;
 import architecture.domain.CountryCodes;
 import architecture.domain.entities.Article;
 import architecture.domain.entities.Category;
-import architecture.domain.entities.Image;
 import architecture.domain.models.bindingModels.ImageBindingModel;
 import architecture.domain.models.bindingModels.articles.ArticleCreateBindingModel;
 import architecture.repositories.ArticleRepository;
 import architecture.repositories.CategoryRepository;
 import architecture.util.TestConstants;
-import org.apache.commons.text.StringEscapeUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -20,21 +19,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.Cookie;
 import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -86,8 +78,6 @@ public class ArticleControllerIntegrationTests {
 
     @Test
     public void post_createArticle_withValidData_andFullImage_redirectsCorrect() throws Exception {
-        ArticleCreateBindingModel correctBindingModel = this.getCorrectBindingModel();
-        correctBindingModel.setMainImage(this.getCorrectImageBindingModel());
         Long categoryId = this.categoryRepository.findAll().get(0).getId();
 
         this.mockMvc.perform(post("/fr/admin/articles/create")
@@ -95,7 +85,7 @@ public class ArticleControllerIntegrationTests {
                 .contextPath("/fr")
                 .cookie(new Cookie(AppConstants.LOCALE_COOKIE_NAME, "fr"))
                 .param(ViewNames.ARTICLE_CREATE_Category_Id, categoryId.toString())
-                .flashAttr(ViewNames.ARTICLE_CREATE_BindingModel_Name, correctBindingModel))
+                .flashAttr(ViewNames.ARTICLE_CREATE_BindingModel_Name, this.getFullCorrectBindingModel()))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/fr/admin/listAll"))
                 .andDo(print());
@@ -127,8 +117,6 @@ public class ArticleControllerIntegrationTests {
 
     @Test
     public void post_createArticle_withValidData_AndFullImage_storesCorrect() throws Exception {
-        ArticleCreateBindingModel correctBindingModel = this.getCorrectBindingModel();
-        correctBindingModel.setMainImage(this.getCorrectImageBindingModel());
         Long categoryId = this.categoryRepository.findAll().get(0).getId();
 
         this.mockMvc.perform(post("/fr/admin/articles/create")
@@ -136,7 +124,7 @@ public class ArticleControllerIntegrationTests {
                 .contextPath("/fr")
                 .cookie(new Cookie(AppConstants.LOCALE_COOKIE_NAME, "fr"))
                 .param(ViewNames.ARTICLE_CREATE_Category_Id, categoryId.toString())
-                .flashAttr(ViewNames.ARTICLE_CREATE_BindingModel_Name, correctBindingModel));
+                .flashAttr(ViewNames.ARTICLE_CREATE_BindingModel_Name, this.getFullCorrectBindingModel()));
 
         Article storedArticle = this.articleRepository.findAll().get(0);
 
@@ -194,6 +182,8 @@ public class ArticleControllerIntegrationTests {
     }
 
     //Test post when all data is valid except one invalid field
+
+    //Country
     @Test
     public void post_createArticle_withNullCountry_returnsForm() throws Exception {
         ArticleCreateBindingModel invalidModel = this.getCorrectBindingModel();
@@ -230,6 +220,38 @@ public class ArticleControllerIntegrationTests {
                 .andDo(print());
     }
 
+    //Url
+    @Test
+    public void post_createArticle_withNullUrl_returnsForm() throws Exception {
+        ArticleCreateBindingModel invalidModel = this.getCorrectBindingModel();
+        invalidModel.getMainImage().setUrl(null);
+        this.mockMvc.perform(post("/fr/admin/articles/create")
+                .locale(Locale.FRANCE)
+                .contextPath("/fr")
+                .cookie(new Cookie(AppConstants.LOCALE_COOKIE_NAME, "fr"))
+                .param(ViewNames.ARTICLE_CREATE_Category_Id, "")
+                .flashAttr(ViewNames.ARTICLE_CREATE_BindingModel_Name, invalidModel))
+                .andExpect(status().isOk())
+                .andExpect(model().errorCount(1))
+                .andDo(print());
+    }
+
+    @Test
+    public void post_createArticle_invalidUrl_returnsForm() throws Exception {
+        ArticleCreateBindingModel invalidModel = this.getFullCorrectBindingModel();
+        invalidModel.getMainImage().setUrl("invalidUrl");
+        this.mockMvc.perform(post("/fr/admin/articles/create")
+                .locale(Locale.FRANCE)
+                .contextPath("/fr")
+                .cookie(new Cookie(AppConstants.LOCALE_COOKIE_NAME, "fr"))
+                .param(ViewNames.ARTICLE_CREATE_Category_Id, "")
+                .flashAttr(ViewNames.ARTICLE_CREATE_BindingModel_Name, invalidModel))
+                .andExpect(status().isOk())
+                .andExpect(model().errorCount(2))
+                .andExpect(model().attributeHasFieldErrorCode(ViewNames.ARTICLE_CREATE_BindingModel_Name, "mainImage.url", ImageBindingValidationEmpty.class.getSimpleName() ))
+                .andDo(print());
+    }
+
     private ArticleCreateBindingModel getCorrectBindingModel(){
         ArticleCreateBindingModel model = new ArticleCreateBindingModel();
         model.setTitle(TestConstants.ARTICLE_VALID_TITLE);
@@ -246,6 +268,13 @@ public class ArticleControllerIntegrationTests {
         imageBindingModel.setUrl(TestConstants.IMAGE_URL);
         imageBindingModel.setName(TestConstants.IMAGE_FR_NAME_2);
         return imageBindingModel;
+    }
+
+    private ArticleCreateBindingModel getFullCorrectBindingModel(){
+        ArticleCreateBindingModel correctBindingModel = this.getCorrectBindingModel();
+        correctBindingModel.getMainImage().setUrl(TestConstants.IMAGE_URL);
+        correctBindingModel.getMainImage().setName(TestConstants.IMAGE_FR_NAME_2);
+        return correctBindingModel;
     }
 
     private void seedCategory(){
