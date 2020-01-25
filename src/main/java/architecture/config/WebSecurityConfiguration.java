47ab060a797a20ac6bd2607933fa1c5e70ce80d6
warumNicht <andrey_st_minev@yahoo.com>
2020-01-25 13:42:28 +0200

    filter to redirect to login with locale context path

--- src/main/java/architecture/config/WebSecurityConfiguration.java
+++ src/main/java/architecture/config/WebSecurityConfiguration.java
@@ -1,12 +1,20 @@
 package architecture.config;
 
+import architecture.web.interceptors.CustomLoginFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.security.web.session.ConcurrentSessionFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,21 +27,23 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .csrfTokenRepository(this.csrfTokenRepository())
                 .and()
                 .authorizeRequests()
-                .antMatchers("/","/{en|de|fr}/", "/{en|de|fr}/users/login", "/{en|de|fr}/users/register").permitAll()
                 .antMatchers( "/{en|de|fr}/admin/**").access("hasRole('ADMIN')")
-
-
+                .antMatchers("/","/{en|de|fr}/", "/{en|de|fr}/users/login","/users/login", "/{en|de|fr}/users/register").permitAll()
                 .antMatchers("/css/**", "/js/**", "/favicon/**","/images/**","/fetch/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
+                .addFilterBefore(new CustomLoginFilter(), ChannelProcessingFilter.class)
                 .formLogin()
+                .loginProcessingUrl("/{en|de|fr}/users/login")
 
-//                .loginProcessingUrl("/{en|de|fr}/users/login")
-                .loginPage("/en/users/login")
+                .loginPage("/es/users/login")
+
                 .usernameParameter("username")
                 .passwordParameter("password")
                 .defaultSuccessUrl("/home")
                 .and()
+
+
                 .logout().logoutSuccessUrl("/")
                 .and()
                 .exceptionHandling().accessDeniedPage("/unauthorized");
---
+++ src/main/java/architecture/web/interceptors/CustomLoginFilter.java
@@ -0,0 +1,41 @@
+package architecture.web.interceptors;
+
+import architecture.constants.AppConstants;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class CustomLoginFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(
+            ServletRequest request,
+            ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        if(httpServletRequest.getRequestURI().contains("admin")){
+            SecurityContext context = SecurityContextHolder.getContext();
+            Authentication authentication = context.getAuthentication();
+            if(authentication==null){
+                String locale = Arrays.stream(httpServletRequest.getCookies())
+                        .filter(c -> c.getName().equals(AppConstants.LOCALE_COOKIE_NAME))
+                        .findFirst().orElse(null).getValue();
+                HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+                httpServletResponse.sendRedirect(String.format("/%s/users/login",locale));
+                return;
+            }
+
+        }
+        chain.doFilter(request, response);
+    }
+}
