aecee57815c388af383a2d93d42c7a1ebc851f55
warumNicht <andrey_st_minev@yahoo.com>
2020-02-03 18:47:15 +0200

    corrected CustomAccessDeniedHandler

--- src/main/java/architecture/error/CustomAccessDeniedHandler.java
+++ src/main/java/architecture/error/CustomAccessDeniedHandler.java
@@ -1,9 +1,12 @@
 package architecture.error;
 
+import architecture.constants.AppConstants;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.web.util.WebUtils;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -12,8 +15,11 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
                        AccessDeniedException e) throws IOException, ServletException {
-
+        Cookie actualCookie = WebUtils.getCookie(request, AppConstants.LOCALE_COOKIE_NAME);
+        String localeContext= actualCookie!=null ? actualCookie.getValue() : "en";
         request.getSession().setAttribute("accessDeniedException", e);
-        response.sendRedirect(request.getContextPath() + "/unauthorized");
+        String redirectedUrl = String.format("%s/%s/%s",
+                request.getContextPath(), localeContext, "unauthorized");
+        response.sendRedirect(redirectedUrl);
     }
 }
