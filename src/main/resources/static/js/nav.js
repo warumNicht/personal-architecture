b6d6f012047f245cbf94ae7051e1b8e9beac7a9e
warumNicht <andrey_st_minev@yahoo.com>
2020-03-15 18:23:24 +0200

    icons in dropdown

--- src/main/resources/static/js/nav.js
+++ src/main/resources/static/js/nav.js
@@ -1,12 +1,18 @@
 import {fetchCategories, getLocale} from "./fetch-functions.js";
 
+//test for touch events support and if not supported, attach .no-touch class to the HTML tag.
+
+if (!("ontouchstart" in document.documentElement)) {
+    document.documentElement.className += " no-touch";
+}
+
 $('ul.navbar-nav li.dropdown').hover(function () {
     $(this).find(".dropdown-menu").stop(true, true).delay(200).fadeIn(500);
 }, function () {
     $(this).find(".dropdown-menu").stop(true, true).delay(200).fadeOut(500);
 });
 
-$('ul.navbar-nav').find('select').click(function (e) {
+$('ul.navbar-nav').find('.dropdown').click(function (e) {
     e.stopPropagation();
 });
 
