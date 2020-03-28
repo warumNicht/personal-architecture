623b4aad3fbce2a572a6ca52693fab73ebdc1417
warumNicht <andrey_st_minev@yahoo.com>
2020-03-28 18:47:18 +0200

    removed

--- src/main/resources/static/js/nav.js
+++ src/main/resources/static/js/nav.js
@@ -1,15 +1,5 @@
 import {fetchCategories, getLocale, fetchCategoriesDropdown} from "./fetch-functions.js";
 
-
-
-
-
-//test for touch events support and if not supported, attach .no-touch class to the HTML tag.
-
-if (!("ontouchstart" in document.documentElement)) {
-    document.documentElement.className += " no-touch";
-}
-
 $('ul.navbar-nav li.dropdown').hover(function () {
     $(this).find(".dropdown-menu").stop(true, true).delay(200).fadeIn(500);
 }, function () {
@@ -21,21 +11,6 @@ $('ul.navbar-nav').find('.dropdown').click(function (e) {
 });
 
 $(document).ready(function () {
-
-    // $("#select-locales").change(function () {
-    //     let selectedOption = $('#select-locales').val();
-    //     if (selectedOption !== '') {
-    //         location.replace('?lang=' + selectedOption.toLowerCase());
-    //     }
-    // });
-
-    // $('.dropdown').each(function (){
-    //     this.addEventListener('click',function (event){
-    //         console.log(event);
-    //
-    //         $(this).find( 'div.dropdown-content' ).css({display:"block"})
-    //     });
-    // });
 
     document.querySelectorAll('.dropdown').forEach(function(dropdown){
         const currentLang=dropdown.querySelector('.dropdown-icon');
