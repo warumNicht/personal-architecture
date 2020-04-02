22a981dc59afa7a5477c605723249d5b621f62ac
warumNicht <andrey_st_minev@yahoo.com>
2020-04-02 19:37:14 +0300

    added svg to country enum

--- src/main/java/architecture/domain/CountryCodes.java
+++ src/main/java/architecture/domain/CountryCodes.java
@@ -1,9 +1,24 @@
 package architecture.domain;
 
 public enum CountryCodes {
-    BG,
-    FR,
-    DE,
-    ES,
-    EN
+
+    BG("<svg viewBox=\"0 0 26 16\">\n" +
+                "<rect width=\"8.67\" height=\"16\" style=\"fill:#2b3379\"></rect>\n" +
+                "<rect x=\"8.67\" width=\"8.67\" height=\"16\" style=\"fill:#f4d130\"></rect>\n" +
+                "<rect x=\"17.33\" width=\"8.67\" height=\"16\" style=\"fill:#bd1f34\"></rect>\n" +
+            "</svg>"),
+    FR("bg"),
+    DE("bg"),
+    ES("bg"),
+    EN("bg");
+
+    private String svg;
+
+    private CountryCodes(String svg) {
+        this.svg = svg;
+    }
+
+    public String getSvg() {
+        return svg;
+    }
 }
