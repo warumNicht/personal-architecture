406854b0574bca7ddff8c769a20a2f411715a6f2
warumNicht <andrey_st_minev@yahoo.com>
2020-04-02 19:42:59 +0300

    bg flag

--- src/main/java/architecture/domain/CountryCodes.java
+++ src/main/java/architecture/domain/CountryCodes.java
@@ -3,11 +3,15 @@ package architecture.domain;
 public enum CountryCodes {
 
     BG("<svg viewBox=\"0 0 26 16\">\n" +
-                "<rect width=\"8.67\" height=\"16\" style=\"fill:#2b3379\"></rect>\n" +
-                "<rect x=\"8.67\" width=\"8.67\" height=\"16\" style=\"fill:#f4d130\"></rect>\n" +
-                "<rect x=\"17.33\" width=\"8.67\" height=\"16\" style=\"fill:#bd1f34\"></rect>\n" +
+                "<rect width=\"26\" height=\"16\" style=\"fill:#fff\"></rect>\n" +
+                "<rect y=\"5.33\" width=\"26\" height=\"10.67\" style=\"fill:#59b44b\"></rect>\n" +
+                "<rect y=\"10.67\" width=\"26\" height=\"5.33\" style=\"fill:#c42127\"></rect>\n" +
             "</svg>"),
-    FR("bg"),
+    FR("<svg viewBox=\"0 0 26 16\">\n" +
+            "<rect width=\"8.67\" height=\"16\" style=\"fill:#2b3379\"></rect>\n" +
+            "<rect x=\"8.67\" width=\"8.67\" height=\"16\" style=\"fill:#f4d130\"></rect>\n" +
+            "<rect x=\"17.33\" width=\"8.67\" height=\"16\" style=\"fill:#bd1f34\"></rect>\n" +
+            "</svg>"),
     DE("bg"),
     ES("bg"),
     EN("bg");
