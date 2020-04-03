0ff553f2bafcaeeb186c476db6251267cfa57f21
warumNicht <andrey_st_minev@yahoo.com>
2020-04-03 20:01:58 +0300

    german flag

--- src/main/java/architecture/domain/CountryCodes.java
+++ src/main/java/architecture/domain/CountryCodes.java
@@ -9,7 +9,10 @@ public enum CountryCodes {
     FR("<rect width=\"8.67\" height=\"16\" style=\"fill:#0055A4\"></rect>\n" +
             "<rect x=\"8.67\" width=\"8.67\" height=\"16\" style=\"fill:#FFFFFF\"></rect>\n" +
             "<rect x=\"17.33\" width=\"8.67\" height=\"16\" style=\"fill:#EF4135\"></rect>\n"),
-    DE("bg"),
+
+    DE("<rect width=\"26\" height=\"16\" style=\"fill:#000000\"></rect>\n" +
+            "<rect y=\"5.33\" width=\"26\" height=\"10.67\" style=\"fill:#FF0000\"></rect>\n" +
+            "<rect y=\"10.67\" width=\"26\" height=\"5.33\" style=\"fill:#FFCC00\"></rect>\n"),
     ES("bg"),
     EN("bg");
 
