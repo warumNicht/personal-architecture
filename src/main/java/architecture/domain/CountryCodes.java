d2d0434de90a5e332dd408068cee1d680286e1b7
warumNicht <andrey_st_minev@yahoo.com>
2020-04-03 19:56:36 +0300

    removed svg duplication

--- src/main/java/architecture/domain/CountryCodes.java
+++ src/main/java/architecture/domain/CountryCodes.java
@@ -2,16 +2,13 @@ package architecture.domain;
 
 public enum CountryCodes {
 
-    BG("<svg viewBox=\"0 0 26 16\">\n" +
-                "<rect width=\"26\" height=\"16\" style=\"fill:#fff\"></rect>\n" +
-                "<rect y=\"5.33\" width=\"26\" height=\"10.67\" style=\"fill:#59b44b\"></rect>\n" +
-                "<rect y=\"10.67\" width=\"26\" height=\"5.33\" style=\"fill:#c42127\"></rect>\n" +
-            "</svg>"),
-    FR("<svg viewBox=\"0 0 26 16\">\n" +
-            "<rect width=\"8.67\" height=\"16\" style=\"fill:#0055A4\"></rect>\n" +
+    BG("<rect width=\"26\" height=\"16\" style=\"fill:#fff\"></rect>\n" +
+            "<rect y=\"5.33\" width=\"26\" height=\"10.67\" style=\"fill:#59b44b\"></rect>\n" +
+            "<rect y=\"10.67\" width=\"26\" height=\"5.33\" style=\"fill:#c42127\"></rect>\n"),
+
+    FR("<rect width=\"8.67\" height=\"16\" style=\"fill:#0055A4\"></rect>\n" +
             "<rect x=\"8.67\" width=\"8.67\" height=\"16\" style=\"fill:#FFFFFF\"></rect>\n" +
-            "<rect x=\"17.33\" width=\"8.67\" height=\"16\" style=\"fill:#EF4135\"></rect>\n" +
-            "</svg>"),
+            "<rect x=\"17.33\" width=\"8.67\" height=\"16\" style=\"fill:#EF4135\"></rect>\n"),
     DE("bg"),
     ES("bg"),
     EN("bg");
