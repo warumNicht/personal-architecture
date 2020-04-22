305d384d7a81467532d97d85eb667ce9a2b73c1f
warumNicht <andrey_st_minev@yahoo.com>
2020-04-22 21:40:28 +0300

    access control max age 180 sec

--- src/main/java/architecture/web/filters/CorsFilter.java
+++ src/main/java/architecture/web/filters/CorsFilter.java
@@ -25,7 +25,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Max-Age", "180000");
+        response.setHeader("Access-Control-Max-Age", "180");
 //        response.setHeader("Set-Cookie", "HttpOnly; SameSite=Strict");
 
         if("OPTIONS".equals(request.getMethod())){
