2700e1b400c15f9dcbd18885b1756e6ac9569d74
Andrey Minev <andrey_st_minev@yahoo.com>
2020-05-05 23:00:43 +0300

    rest logout method

--- src/main/java/architecture/web/controllers/UserController.java
+++ src/main/java/architecture/web/controllers/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -147,6 +148,18 @@ public class UserController extends BaseController {
             return "redirect:/" + super.getLocale() + attribute;
         }
         return "redirect:/" + super.getLocale() + "/";
+    }
+
+    @RequestMapping(value="/custom-logout", method = RequestMethod.GET)
+    public String customLogout(HttpServletRequest request, HttpServletResponse response) {
+        // Get the Spring Authentication object of the current request.
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // In case you are not filtering the users of this request url.
+        if (authentication != null){
+            new SecurityContextLogoutHandler().logout(request, response, authentication); // <= This is the call you are looking for.
+        }
+        return "redirect:/login-page";
     }
 
     @PostConstruct
