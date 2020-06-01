08a325c1987297d70dc096d92fb97321146424a9
Andrey Minev <andrey_st_minev@yahoo.com>
2020-06-01 14:41:14 +0300

    token validity 1 h

--- src/main/java/architecture/config/jwt/JWTCsrfTokenRepository.java
+++ src/main/java/architecture/config/jwt/JWTCsrfTokenRepository.java
@@ -73,7 +73,7 @@ public class JWTCsrfTokenRepository implements CsrfTokenRepository {
                 .replace("-", "");
 
         Date now = new Date();
-        Date exp = new Date(System.currentTimeMillis() + (1000 * 15 * 1)); // 15 sec
+        Date exp = new Date(System.currentTimeMillis() + (1000 * 60 * 60)); // 1 h
 
         return Jwts.builder()
                 .setId(id)
